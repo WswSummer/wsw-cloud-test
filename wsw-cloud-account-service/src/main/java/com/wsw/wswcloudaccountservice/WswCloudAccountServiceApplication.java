8fca3c7b489ad42f13beb06fbdba2e4b6555a4bf
wangsongwen <2544894086wsw@gmail.com>
2020-10-27 17:10:47 +0800

    账户微服务

---
+++ wsw-cloud-account-service/src/main/java/com/wsw/wswcloudaccountservice/WswCloudAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wsw.wswcloudaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WswCloudAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WswCloudAccountServiceApplication.class, args);
+	}
+
+}
---
+++ wsw-cloud-account-service/src/test/java/com/wsw/wswcloudaccountservice/WswCloudAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsw.wswcloudaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WswCloudAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
