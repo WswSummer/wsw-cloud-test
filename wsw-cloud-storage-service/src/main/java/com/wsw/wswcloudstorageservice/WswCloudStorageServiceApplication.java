49b934a63904df846e15b88ca9d6f0ad0551d1b7
wangsongwen <2544894086wsw@gmail.com>
2020-10-27 17:11:05 +0800

    库存微服务

---
+++ wsw-cloud-storage-service/src/main/java/com/wsw/wswcloudstorageservice/WswCloudStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wsw.wswcloudstorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WswCloudStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WswCloudStorageServiceApplication.class, args);
+	}
+
+}
---
+++ wsw-cloud-storage-service/src/test/java/com/wsw/wswcloudstorageservice/WswCloudStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsw.wswcloudstorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WswCloudStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
