740e13306e1b188d244ba191610f6e556a77f37f
wangsongwen <2544894086wsw@gmail.com>
2020-11-02 10:31:29 +0800

    rabbitmq操作

--- wsw-cloud-order-service/src/main/java/com/wsw/cloudtest/rabbitmq/RabbitService.java
+++ wsw-cloud-order-service/src/main/java/com/wsw/cloudtest/rabbitmq/RabbitService.java
@@ -1,6 +1,28 @@
-package com.wsw.cloudtest.rabbitmq;/**
-* @Author WangSongWen
-* @Date: Created in 10:01 2020/11/2
-* @Description: 
-*/public class RabbitService {
+package com.wsw.cloudtest.rabbitmq;
+
+import com.wsw.cloudtest.domain.Order;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 10:01 2020/11/2
+ * @Description:
+ */
+@Service
+public class RabbitService {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(Order order) throws Exception{
+        Map<String, Object> messageMap = new HashMap<>();
+        messageMap.put("messageId", UUID.randomUUID());
+        messageMap.put("orderMessage", order);
+        rabbitTemplate.convertAndSend("fanoutExchange", null, messageMap);
+    }
 }
